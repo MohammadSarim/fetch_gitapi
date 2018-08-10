d68b771d2894a066833fa81e1d1c5ad1809a21c5
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-10 20:34:09 +0500

    added files

--- script/main.js
+++ script/main.js
@@ -1,6 +1,17 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(function () {
-            console.log('Service Worker Registered');
-        });
+
+window.addEventListener("load" , getName);
+
+function getName(){
+    // var searchname = document.querySelector("#searchname").value;
+//   console.log(userid);
+ fetch(`https://api.github.com/users/mohammadsarim/followers`)
+        .then(function(response) { 
+    return response.json()
+})
+.then(function (data){
+    console.log(data);
+    data.map(function(v,i){
+        
+    })
+})
 }
