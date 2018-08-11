b36ff3a3b3d19ba5714ef7bc9590700a1b8e656a
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-11 16:11:13 +0500

    added work

--- script/main.js
+++ script/main.js
@@ -4,14 +4,47 @@ window.addEventListener("load" , getName);
 function getName(){
     // var searchname = document.querySelector("#searchname").value;
 //   console.log(userid);
+var addpostdata = document.getElementById('addpostdata');
  fetch(`https://api.github.com/users/mohammadsarim/followers`)
         .then(function(response) { 
     return response.json()
 })
-.then(function (data){
-    console.log(data);
-    data.map(function(v,i){
-        
+    .then(function (data){
+        console.log(data);
+        data.map(function(v,i){
+            console.log(v.login);
+            // addpostdata.innerHTML = 
+            
+            // `
+            // <h1 >${v.login}</h1>
+            // `
+
+
+
+            var addpost = document.createElement("div");
+        addpost.setAttribute("id", "addpost");
+        var img = document.createElement('img');
+        img.setAttribute("src", v.avatar_url);
+        // img.className = "img-fluid ";
+        addpost.appendChild(img);
+        var footer = document.createElement('div');
+        footer.setAttribute("class", "cardfooter");
+        var h4name = document.createElement('h5');
+        var h4namenode = document.createTextNode(v.login);
+        h4name.className = "name";
+        h4name.appendChild(h4namenode);
+        footer.appendChild(h4name);
+     
+        var category = document.createElement('h6');
+        category.className = "category";
+        category.innerHTML = "Category: " + v.id;
+        footer.appendChild(category);
+        var hr = document.createElement('hr');
+        footer.appendChild(hr);
+        addpost.appendChild(footer);
+        addpostdata.appendChild(addpost);
+           
+
     })
 })
 }
